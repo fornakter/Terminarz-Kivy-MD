b4a4dcc04cd068eee56b3c2da3a1334ea90adac2
AdamF <fornakter@gmail.com>
2022-07-23 12:22:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,74 @@
+# Qrcode Barcode Scanner using kivy ,kivymd Python
+
+from kivy.properties import ObjectProperty
+from kivy.clock import mainthread
+from kivy.utils import platform
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivymd.uix.picker import MDDatePicker
+from kivymd.uix.screen import MDScreen
+from camera4kivy import Preview
+from PIL import Image
+from pyzbar.pyzbar import decode
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class MenuScreen(Screen):
+class FirstWindow(Screen):
     pass
 
 
-class ScanScreen(Screen):
+class WindowManager(ScreenManager):
     pass
+
+
+class ScanScreen(MDScreen):
+
+    def on_kv_post(self, obj):
+        try:
+            self.ids.preview.connect_camera(enable_analyze_pixels=True, default_zoom=0.0)
+        except Exception as e:
+            print(e, ' on_kv_post')
+            # Error: 'super' object has no attribute '__getattr__'
+
+        else:
+            self.root.get_screen('secound').ids.preview.connect_camera(enable_analyze_pixels=True, default_zoom=0.0)
+            # Error: AttributeError: 'ScanScreen' object has no attribute 'root'
+
+    @mainthread
+    def got_result(self, result):
+        try:
+            self.ids.ti.text = str(result)
+        except Exception as e:
+            print(e, ' got_result')
+        else:
+            self.root.get_screen('secound').ids.ti.text = str(result)
+        print(result[0])
 
 
 sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(ScanScreen())
+sm.add_widget(FirstWindow(name='first'))
+sm.add_widget(ScanScreen(name='secound'))
 
 
-class Scanner(MDApp):
+class ScanAnalyze(Preview):
+    extracted_data = ObjectProperty(None)
+
+    def analyze_pixels_callback(self, pixels, image_size, image_pos, scale, mirror):
+        pimage = Image.frombytes(mode='RGBA', size=image_size, data=pixels)
+        list_of_all_barcodes = decode(pimage)
+
+        if list_of_all_barcodes:
+            if self.extracted_data:
+                self.extracted_data(list_of_all_barcodes[0])
+            else:
+                print("Not found")
+
+
+class QRScan(MDApp):
     def build(self):
-        return Builder.load_file('date.kv')
-
-    def on_save(self, instance, value, date_range):
-        self.root.ids.date_button.text = str(value)
-
-    def on_cancel(self, instance, value):
-        self.root.ids.date_label.text = 'Anulacja'
-
-    def show_date_picker(self):
-        date_dialog = MDDatePicker()
-        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        date_dialog.open()
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.CAMERA, Permission.RECORD_AUDIO])
+        return ScanScreen()
 
 
-Scanner().run()
+if __name__ == '__main__':
+    QRScan().run()
